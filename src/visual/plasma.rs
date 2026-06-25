43594b15eee9c8ac302a548730bda595e9ad586d
ppmuzyk <przemyslaw.muzyk@gmail.com>
2026-06-25 19:47:10 +0200

    chore: remove unused smoothstep from plasma.rs after refactor

--- src/visual/plasma.rs
+++ src/visual/plasma.rs
@@ -134,11 +134,6 @@ void main() {
 }
 "#;
 
-fn smoothstep(edge0: f32, edge1: f32, x: f32) -> f32 {
-    let t = ((x - edge0) / (edge1 - edge0)).clamp(0.0, 1.0);
-    t * t * (3.0 - 2.0 * t)
-}
-
 
 
 
